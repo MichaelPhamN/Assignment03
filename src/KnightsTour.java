86631a3567b0e15ec0e91b1c766074b2e43ade38
Admin <Admin@10.20.14.187>
2017-04-28 13:52:48 -0700

    k

---
+++ src/KnightsTour.java
@@ -0,0 +1,88 @@
+import java.util.Arrays;
+
+public class KnightsTour {
+
+	private static boolean tryMove(int[][] board, int newX, int newY, int move) {
+		if (newX < 0 || newX >= board.length || 
+			newY < 0 || newY >= board[newX].length) {
+			return false; // Move would be out of bounds
+		}
+		if (board[newX][newY] != 0) {
+			// Already moved there, not allowed
+			return false;
+		}
+		// Make the move (since we can)
+		board[newX][newY] = move;
+		// Recursively try to solve from here
+		if (solve(board, newX, newY, move)) {
+			return true;
+		}
+		// Solve didn't succeed
+		board[newX][newY] = 0; // Undo the move
+		return false; // Unsuccessful
+	}
+	
+	public static boolean solve(int[][] board, int x, int y, int move) {
+		if (board.length*board[0].length == move) {
+			return true; // We have solved it!
+		}
+		move++; // Next move
+		// x - 2, y - 1
+		if (tryMove(board, x - 2, y - 1, move)) {
+			return true;
+		}
+		// x - 2, y + 1
+		if (tryMove(board, x - 2, y + 1, move)) {
+			return true;
+		}
+		// x - 1, y - 2
+		if (tryMove(board, x - 1, y - 2, move)) {
+			return true;
+		}
+		// x - 1, y + 2
+		if (tryMove(board, x - 1, y + 2, move)) {
+			return true;
+		}
+
+		// x + 2, y - 1
+		if (tryMove(board, x + 2, y - 1, move)) {
+			return true;
+		}
+		// x + 2, y + 1
+		if (tryMove(board, x + 2, y + 1, move)) {
+			return true;
+		}
+		// x + 1, y - 2
+		if (tryMove(board, x + 1, y - 2, move)) {
+			return true;
+		}
+		// x + 1, y + 2
+		if (tryMove(board, x + 1, y + 2, move)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		int n = 5;
+		int[][] board = new int[n][n];
+		
+		for (int x = 0; x < n; x++) {
+			for (int y = 0; y < n; y++) {
+				// Start the knight at (x, y)
+				board[x][y] = 1;
+				if (solve(board, x, y, 1)) {
+					for (int i = 0; i < board.length; i++) {
+						System.out.println(Arrays.toString(board[i]));
+					}
+					return;
+				} else {
+					board[x][y] = 0;
+				}
+			}
+		}
+		
+
+	}
+
+}
--- src/PegSolitaire.java
+++ src/PegSolitaire.java
@@ -1,89 +1,135 @@
 
 public class PegSolitaire {	
 	public static boolean solve(boolean[][] pegs, StringStack solution){
-		for(int i = 0; i < pegs.length; i++){
-			for(int j = 0; j < pegs[i].length; j++){
-				int newX = 0, newY = 0;
-				// Jump Up
-				if(tryMove(pegs, i, j, 0, 2, newX, newY, solution)){
-					return true;
-				}
-				
-				// Jump Down
-				if(tryMove(pegs, i, j, 0, -2, newX, newY, solution)){
-					return true;
-				}
-				
-				// Jump Left
-				if(tryMove(pegs, i, j, -2, 0, newX, newY, solution)){
-					return true;
-				}
-				
-				// Jump Right
-				if(tryMove(pegs, i, j, 2, 0, newX, newY, solution)){
-					return true;
-				}
-			}
-		}
+		int startX = 0;
+		int startY = 0;
 		
-		
-		
+		if(solve(pegs, startX, startY,))
 		
 		return false;
 		
 	}
+	
+	public static boolean solve(boolean[][] pegs, int startX, int startY, int endX, int endY, StringStack solution){
+		// Jump Up
+		if(tryMove(pegs, startX, startY, 0, 2, endX, endY, solution)){
+			return true;
+		}
+		
+		// Jump Down
+		if(tryMove(pegs, startX, startY, 0, -2, endX, endY, solution)){
+			return true;
+		}
+		
+		// Jump Left
+		if(tryMove(pegs, startX, startY, -2, 0, endX, endY, solution)){
+			return true;
+		}
+		
+		// Jump Right
+		if(tryMove(pegs, startX, startY, 2, 0, endX, endY,solution)){
+			return true;
+		}
+	}
 	private static boolean tryMove(boolean[][] pegs, int startX, int startY, int jumpX, int jumpY, int endX, int endY, StringStack solution){
+		// Move out of bound
 		if(endX < 0 || endX >= pegs.length 
 				|| endY < 0 || endY >= pegs.length)
 		{
 			return false;
 		}
 		
-		
-		
-		// (0,0)
+		// Jump to point (0,0)		 
 		if(endX == 0 && endY == 0){
 			return false;
 		}
 		
-		// (0, 1)
+		// Jump to point (0,1)
 		if(endX == 0 && endY == 1){
 			return false;
 		}
 		
-		// (1,0)
+		// Jump to point (1,0)
 		if(endX == 1 && endY == 0){
 			return false;
 		}
 		
-		// (1, 1)
+		// Jump to point (1, 1)
 		if(endX == 1 && endY == 1){
 			return false;
 		}
 		
-		// (0,0)
+		// Jump to point (7,7)
 		if(endX == 7 && endY == 7){
 			return false;
 		}
 		
-		// (0, 1)
+		// Jump to point (7, 6)
 		if(endX == 7 && endY == 6){
 			return false;
 		}
 		
-		// (1,0)
+		// Jump to point (6,7)
 		if(endX == 6 && endY == 7){
 			return false;
 		}
 		
-		// (1, 1)
+		// Jump to point (6, 6)
 		if(endX == 6 && endY == 6){
 			return false;
 		}
 		
 		
+		// Jump to point (7,0)
+		if(endX == 7 && endY == 0){
+			return false;
+		}
 		
+		// Jump to point (7, 1)
+		if(endX == 7 && endY == 1){
+			return false;
+		}
 		
+		// Jump to point (6,0)
+		if(endX == 6 && endY == 0){
+			return false;
+		}
+		
+		// Jump to point (6, 1)
+		if(endX == 6 && endY == 1){
+			return false;
+		}
+		
+		// Jump to point (0,7)
+		if(endX == 0 && endY == 7){
+			return false;
+		}
+		
+		// Jump to point (0, 6)
+		if(endX == 0 && endY == 6){
+			return false;
+		}
+		
+		// Jump to point (1,7)
+		if(endX == 1 && endY == 7){
+			return false;
+		}
+		
+		// Jump to point (1, 6)
+		if(endX == 1 && endY == 6){
+			return false;
+		}		
+		
+		// Check if Jump to point (3,3)
+		/* If all points equal true except for (0,0),(0,1),(1,0),(1,1),(7,7),(7,6),(7,7)
+		 * (6,6), (0,7), (0,6), (1,7), (1,6), (7,0), (6,0), (7,1), (6,1) => return true (finish)
+		 * else // not succeed
+		 *  run again from start X, startY with different direction
+		 */
+		
+		if(endX == 3 && endY == 3){
+			
+		}
 		
 		return false;
 		
