9866847f1d251db26ff097ec23d26bd983c0a8f6
admin <admin@10.1.10.149>
2017-05-24 18:18:44 -0700

    a

--- src/PegSolitaire.java
+++ src/PegSolitaire.java
@@ -27,6 +27,14 @@ public class PegSolitaire {
 		}
 		
 		// For each board position. Try moving up, down, left, right 
+		if(tryMove(pegs, newX, newY, newX, newY - 1, newX, newY - 2,solution)){
+			return true;
+		}
+		
+		if(tryMove(pegs, newX, newY, newX, newY + 1, newX, newY + 2,solution)){
+			return true;
+		}
+		
 		if(tryMove(pegs, newX, newY, newX - 1, newY, newX - 2,newY,solution)){
 			return true;
 		}
@@ -35,19 +43,11 @@ public class PegSolitaire {
 			return true;
 		}
 		
-		if(tryMove(pegs, newX, newY, newX, newY - 1, newX,newY - 2,solution)){
-			return true;
-		}
-		
-		if(tryMove(pegs, newX, newY, newX, newY + 1, newX,newY + 2,solution)){
-			return true;
-		}
-		
 		if (newX < 6){
 			return solve(pegs, newX + 1, newY, solution);
 		}else{
 			return solve(pegs, 0, newY + 1, solution);
-		}
+		}		
 	
 	}
 	
@@ -174,21 +174,29 @@ public class PegSolitaire {
 	}
 	
 	public static void main(String[] args){
-		boolean[][] testSimple = { { false, false, false, false, false, false, false },
-				{ false, false, false, false, false, false, false },
-				{ false, false, false, false, false, false, false },
-				{ false, true, true, false, false, false, false },
-				{ false, false, false, false, false, false, false },
-				{ false, false, false, false, false, false, false },
+		System.out.println("Attempting to solve 'plus' board:");
+		boolean[][] testPlus = { { false, false, false, false, false, false, false },
+				{ false, false, false, true, false, false, false },
+				{ false, false, false, true, false, false, false }, { false, true, true, true, true, true, false },
+				{ false, false, false, true, false, false, false },
+				{ false, false, false, true, false, false, false },
 				{ false, false, false, false, false, false, false } };
-		String simpleSolution = getSolution(testSimple);
-		if ("(1, 3) -> (3, 3)".equals(simpleSolution) || "(3, 1) -> (3, 3)".equals(simpleSolution)) {
-			System.out.println("Your code correctly found the solution to a simple board! 10/10");			
+		String plusSolution = getSolution(testPlus);
+		if (plusSolution != null) {
+			String plusSolution1 = "(3, 2) -> (3, 0), (3, 4) -> (3, 2), (1, 3) -> (3, 3), (3, 3) -> (3, 1), (3, 0) -> (3, 2), (5, 3) -> (3, 3), (3, 2) -> (3, 4), (3, 5) -> (3, 3)";
+			String plusSolution2 = "(2, 3) -> (0, 3), (4, 3) -> (2, 3), (3, 1) -> (3, 3), (3, 3) -> (1, 3), (0, 3) -> (2, 3), (3, 5) -> (3, 3), (2, 3) -> (4, 3), (5, 3) -> (3, 3)";
+			if (plusSolution1.equals(plusSolution)) {
+				System.out.println("Correct solution to the plus board! 30/30");
+//				points += 30;
+			} else if (plusSolution2.equals(plusSolution)) {
+				System.out.println("It looks like you're scanning column by column instead of row by row! 25/30");
+//				points += 25;
+			} else {
+				System.out.println("Unexpected solution! 0/30");
+				System.out.println(plusSolution);
+			}
 		} else {
-			System.out.println(
-					"Your code couldn't find a solution to a simple board requiring one move, so I'm exiting now with 0 points.");
-			System.out.println("Solution seen: "+simpleSolution);
-			return;
+			System.out.println("Your code didn't find a solution! 0/30");
 		}
 	}
 
