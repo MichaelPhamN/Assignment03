3189a4bc035c2348fc7ac565cf6696ca94e699b2
Admin <Admin@10.0.0.8>
2017-05-26 01:52:12 -0700

    finish

--- src/PegSolitaire.java
+++ src/PegSolitaire.java
@@ -35,11 +35,11 @@ public class PegSolitaire {
 		}
 		
 		if (newX < 6){
-			solve(pegs, newX + 1, newY, solution);
+			return solve(pegs, newX + 1, newY, solution);
 		}else{
-			solve(pegs, 0, newY + 1, solution);
-		}		
-		return false;
+			return solve(pegs, 0, newY + 1, solution);
+						
+		}
 		
 	}
 	
@@ -95,7 +95,7 @@ public class PegSolitaire {
 		}
 		
 		// (1,5)
-		if(endX == 5 && endY == 1){
+		if(endX == 1 && endY == 5){
 			return false;
 		}
 		
@@ -129,34 +129,22 @@ public class PegSolitaire {
 			return false;
 		}
 		
-		// Dung
-		if(pegs[endX][endY] == false
-                && pegs[jumpX][jumpY] == true
-                && pegs[startX][startY] == true){
+		if (pegs[endX][endY] == false && pegs[jumpX][jumpY] == true && pegs[startX][startY] == true) {
 			pegs[endX][endY] = true;
 			pegs[startX][startY] = false;
-			pegs[jumpX][jumpY] = false;		
-			String str = "(" + startX + ", " + startY + ") -> (" + endX + ", " + endY +")";
-			System.out.println("jump " + str);
-			solution.push(str);		
-			for (int i = 0; i < pegs.length; i++) {
-				for (int j = 0; j < pegs.length; j++) {
-					if(pegs[i][j]  == false)
-						System.out.print("false ");
-					else
-						System.out.print("true ");
-				}
-				System.out.println();
+			pegs[jumpX][jumpY] = false;	
+			String str = "(" + startX + ", " + startY + ") -> (" + endX + ", " + endY +")";			
+			solution.push(str);
+			// Recursively try to solve from here
+			if(solve(pegs, solution)){
+			return true;
 			}
+			// Undo the move
+			pegs[endX][endY] = false;
+			pegs[startX][startY] = true;
+			pegs[jumpX][jumpY] = true;	
+			solution.pop();
 		}
-		
-		
-		
-//		 Undo the move		
-//		pegs[endX][endY] = false;
-//		pegs[startX][startY] = true;
-//		pegs[jumpX][jumpY] = true;			
-//		solution.pop();
 		
 		return false;
 	}
@@ -171,47 +159,6 @@ public class PegSolitaire {
 			}
 		}
 		return count;
-	}
-	
-	
-	public static String getSolution(boolean[][] board) {
-		StringStack stack = new StringStack();
-		if (PegSolitaire.solve(board, stack)) {
-			String cat = stack.pop();
-			String s;
-			while ((s = stack.pop()) != null) {
-				cat = s + ", " + cat;
-			}
-			return cat;
-		}
-		return null;
-	}
-	
-	public static void main(String[] args){
-		System.out.println("Attempting to solve 'plus' board:");
-		boolean[][] testPlus = { 
-				{ false, false, false, false, false, false, false },
-				{ false, false, false, true, false, false, false },
-				{ false, false, false, true, false, false, false }, 
-				{ false, true, true, true, true, true, false },
-				{ false, false, false, true, false, false, false },
-				{ false, false, false, true, false, false, false },
-				{ false, false, false, false, false, false, false } };
-		String plusSolution = getSolution(testPlus);
-		if (plusSolution != null) {
-			String plusSolution1 = "(3, 2) -> (3, 0), (3, 4) -> (3, 2), (1, 3) -> (3, 3), (3, 3) -> (3, 1), (3, 0) -> (3, 2), (5, 3) -> (3, 3), (3, 2) -> (3, 4), (3, 5) -> (3, 3)";
-			String plusSolution2 = "(2, 3) -> (0, 3), (4, 3) -> (2, 3), (3, 1) -> (3, 3), (3, 3) -> (1, 3), (0, 3) -> (2, 3), (3, 5) -> (3, 3), (2, 3) -> (4, 3), (5, 3) -> (3, 3)";
-			if (plusSolution1.equals(plusSolution)) {
-				System.out.println("Correct solution to the plus board! 30/30");				
-			} else if (plusSolution2.equals(plusSolution)) {
-				System.out.println("It looks like you're scanning column by column instead of row by row! 25/30");				
-			} else {
-				System.out.println("Unexpected solution! 0/30");
-				System.out.println(plusSolution);
-			}
-		} else {
-			System.out.println("Your code didn't find a solution! 0/30");
-		}
 	}
 
 }
